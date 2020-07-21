d33ee35a80a20571b88b63f906e76dd0c25adce8
Summer Duke <esrduke@gmail.com>
2020-07-20 20:58:49 -0500

    added arrays.js

---
+++ arrays.js
@@ -0,0 +1,46 @@
+// console.log('arrays');
+
+//SPLIT
+
+// const bestFriend = "brookie";
+// const bestFriendArray = bestFriend.split('');
+
+// console.log('my best friend in array format', bestFriendArray);
+
+//JOIN
+
+// const array2 = ['cow', 'dog', 'cat'];
+// const string = array2.join(", ");
+// console.log(string);
+
+//POP
+
+const animals = ['dog', 'cat'];
+// const lastAnimal = animals.pop();
+// console.log('animals', animals);
+// console.log("last animal", lastAnimal);
+
+// console.log(typeof lastAnimal);
+
+//SHIFT
+
+// const first = animals.shift();
+// console.log(first);
+// console.log("animals", animals);
+
+//UNSHIFT
+
+// animals.unshift('bear');
+// console.log(animals);
+
+// animals.push('cow');
+// console.log(animals);
+
+let names = ['terry', 'john', 'arnold', 'greg', 'patty'];
+
+if (names.includes('greg')) {
+    console.log('greg is here!');
+}
+else {
+    console.log('greg aint here');
+}
